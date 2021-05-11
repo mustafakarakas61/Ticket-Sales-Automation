8d025e0392bd292c4754670b4fce7f756ccb49be
yakup <yakup@MrJC>
2021-05-11 19:31:10 +0300

    asd

--- RastgeleButon/src/AnaEkran.java
+++ RastgeleButon/src/AnaEkran.java
@@ -27,7 +27,7 @@ import javax.swing.border.SoftBevelBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.border.BevelBorder;
-// herkese selam 
+// herkese selam as
 public class AnaEkran extends JFrame {
 
 	private JPanel contentPane;
