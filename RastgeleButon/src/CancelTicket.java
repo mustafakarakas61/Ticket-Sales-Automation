5919c930ec3ca1872a96935f6906e29d6756cd10
musta <musta@ASUS-MUSTAFA>
2021-05-19 16:23:05 +0300

    CAncel

--- RastgeleButon/src/CancelTicket.java
+++ RastgeleButon/src/CancelTicket.java
@@ -19,10 +19,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JComboBox;
 
 public class CancelTicket extends JFrame {
-
+//comboBox_UserSeats.addItem(sh.getMemberSeat(mmbr.getID())+"");
 	private JPanel contentPane;
-	private JTextField textField;
-
+	public static JLabel lbl_SeatsMember;
 	/**
 	 * Launch the application.
 	 */
@@ -45,9 +44,10 @@ public class CancelTicket extends JFrame {
 	public CancelTicket() {
 		SeatHelper sh = new SeatHelper();
 		Member  mmbr = new Member();
-	    JComboBox comboBox_UserSeats;
+		
+		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 453, 242);
+		setBounds(100, 100, 453, 262);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(240, 230, 140));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -57,7 +57,7 @@ public class CancelTicket extends JFrame {
 		JLabel lbl_Info = new JLabel("Iptal etmek istediginiz koltugun numarasini secin:");
 		lbl_Info.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_Info.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lbl_Info.setBounds(10, 27, 414, 55);
+		lbl_Info.setBounds(10, 53, 414, 55);
 		contentPane.add(lbl_Info);
 		
 		JButton btn_cancel = new JButton("Iptal et");
@@ -66,36 +66,21 @@ public class CancelTicket extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btn_cancel.setBounds(127, 131, 177, 55);
+		btn_cancel.setBounds(127, 157, 177, 55);
 		contentPane.add(btn_cancel);
 		
 		JLabel lbl_UserSeats = new JLabel("Satin aldiginiz koltuklar:");
 		lbl_UserSeats.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_UserSeats.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lbl_UserSeats.setBounds(20, 72, 209, 55);
+		lbl_UserSeats.setBounds(20, 98, 209, 55);
 		contentPane.add(lbl_UserSeats);
 		
-		 comboBox_UserSeats = new JComboBox();
-		comboBox_UserSeats.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				
-				try {
-					comboBox_UserSeats.addItem(sh.getMemberSeat(mmbr.getID())+"");
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-				
-			}
-		});
-		comboBox_UserSeats.setBounds(227, 84, 76, 30);
-		contentPane.add(comboBox_UserSeats);
 		
-		textField = new JTextField();
-		textField.setBounds(34, 11, 370, 30);
-		contentPane.add(textField);
-		textField.setColumns(10);
+	    lbl_SeatsMember = new JLabel("");
+		lbl_SeatsMember.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_SeatsMember.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
+		lbl_SeatsMember.setBounds(10, 11, 414, 44);
+		contentPane.add(lbl_SeatsMember);
+		
 	}
 }
