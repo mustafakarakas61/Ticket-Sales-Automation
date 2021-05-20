85b41ba5641cae9a23575a1cfee394bb4ce9581a
musta <musta@ASUS-MUSTAFA.mshome.net>
2021-05-20 09:47:41 +0300

    ItemChangeListener Tamam

--- RastgeleButon/src/Packed/ItemChangeListener.java
+++ RastgeleButon/src/Packed/ItemChangeListener.java
@@ -1,14 +1,15 @@
 package Packed;
+
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class ItemChangeListener implements ItemListener{
+public class ItemChangeListener implements ItemListener {
 
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		if (e.getStateChange() == ItemEvent.SELECTED) {
-	          Object item = e.getItem();
-		}		
+			Object item = e.getItem();
+		}
 	}
 
 }
